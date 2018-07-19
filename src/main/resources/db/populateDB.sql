b90265b1064304abb24b5d437abada388a2c03ed
Andrew <andrey.mtu@yandex.ru>
2018-07-19 21:06:57 +0300

    Added meal table scrips

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -9,3 +9,8 @@ INSERT INTO users (name, email, password) VALUES
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (user_id, timestamp, description)  VALUES
+  (100000, '2015-05-30 10:0:0', 'Завтрак'),
+  (100000, '2015-05-30 13:0:0', 'Обед'),
+  (100000, '2015-05-30 20:0:0', 'Ужин');
